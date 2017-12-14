3441e61bdd47d782213c6f2a3e7eea3a96fce308
Álvaro Tihanyi <34547477+krauzeSD@users.noreply.github.com>
2017-12-14 18:31:09 +0100

    Basic table structure creation
    First commit related to the MySQL database

---
+++ tablesCreation.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE OnTime;
+USE OnTime;
+CREATE TABLE Sectors
+(
+	Name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (Name)
+);
+CREATE TABLE Business
+(
+	BusinessID VARCHAR(15) NOT NULL,    
+    BusinessName VARCHAR(50) NOT NULL,
+    Email VARCHAR(50) NOT NULL,
+    Telephone VARCHAR(15) NOT NULL,
+    Sector VARCHAR(50) NOT NULL,
+    
+    PRIMARY KEY (BusinessID),
+    UNIQUE (Email),
+    FOREIGN KEY (Sector) REFERENCES Sectors(Name)
+);
+CREATE TABLE Establishments
+(
+    BusinessID VARCHAR(15) NOT NULL,    
+    Location VARCHAR(50) NOT NULL,
+    
+    FOREIGN KEY (BusinessID) REFERENCES Business (BusinessID),
+    PRIMARY KEY (BusinessID, Location)  
+);
+CREATE TABLE Individuals
+(
+    Email VARCHAR(50) NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    Surname VARCHAR(50) NOT NULL,
+    Telephone VARCHAR(15) NOT NULL,
+    EncryptedPassword VARCHAR(200) NOT NULL,
+    BusinessID VARCHAR(15) NULL,
+    
+    FOREIGN KEY (BusinessID) REFERENCES Business(BusinessID),
+    PRIMARY KEY (Email)
+);
+CREATE TABLE Appointments
+(
+	Email VARCHAR(50) NOT NULL,
+    BusinessID VARCHAR(15) NOT NULL,
+    Date DATE NOT NULL,
+    Time TIME NOT NULL,
+    Location INT UNSIGNED NOT NULL,
+    
+    FOREIGN KEY (Email) REFERENCES Individuals(Email),
+    FOREIGN KEY (BusinessID) REFERENCES Business(BusinessID),
+    FOREIGN KEY (Location) REFERENCES Establishments(EstablishmentID),
+    PRIMARY KEY (Email, BusinessID)
+);
