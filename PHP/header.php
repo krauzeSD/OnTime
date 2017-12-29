b0c3d205fd2c7e9b72807f73938c0fff7d2be358
Álvaro Tihanyi <34547477+krauzeSD@users.noreply.github.com>
2017-12-29 19:22:37 +0100

    Change links to IMG

--- PHP/header.php
+++ PHP/header.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="icon" href="ontime_logo.png">
+    <link rel="icon" href="../IMG/ontime_logo.png">
 </head>
 <body>
     <div id="header">
-        <a href="index.php"><img id="logo" src="ontime_logo.png" alt="logo"></a>
+        <a href="index.php"><img id="logo" src="../IMG/ontime_logo.png" alt="logo"></a>
         <?php 
             if (!isset($_SESSION)){
                 echo "<span id='motto'>Appointments in no time</span>";
