9c67751f5417d9b3c02fe3af5665097d20e9d7e0
Álvaro Tihanyi <34547477+krauzeSD@users.noreply.github.com>
2018-01-08 14:06:23 +0100

    Login functionality added to the index page
    We finished the login. Now it can distinguish between individuals and companies.

---
+++ PHP/footer.php
@@ -0,0 +1,10 @@
+<div id="footer">
+   <span>Copyright ©
+   <script>
+    var date = new Date();
+    document.write(date.getFullYear())
+    </script>
+       OnTime</span>
+    <a href="https://github.com/krauzeSD/OnTime">GitHub</a>
+    <span class="mention">Logo by Félix Aguilar</span>
+</div>
