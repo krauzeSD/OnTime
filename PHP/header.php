be84b50b4e734e7336912b341d08909fb5ff4f3a
Álvaro Tihanyi <34547477+krauzeSD@users.noreply.github.com>
2018-01-11 17:15:39 +0100

    Add files via upload

--- PHP/header.php
+++ PHP/header.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <link rel="icon" href="../IMG/ontime_logo.png">
+    <link rel="stylesheet" href="../CSS/ontime.css">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa|Mukta+Mahee|Quattrocento+Sans|Quicksand|Ropa+Sans|Rubik|Work+Sans|Yanone+Kaffeesatz" rel="stylesheet">
+    <title>OnTime</title>
+    <script src="../JS/utilities.js"></script>
 </head>
 <body>
     <div id="header">
