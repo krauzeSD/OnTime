53ed29feab6f7493c09ac6d7ac50bc806caa2b76
Álvaro Tihanyi <34547477+krauzeSD@users.noreply.github.com>
2018-01-12 00:23:49 +0100

    Update tablesCreation.sql

--- SQL/tablesCreation.sql
+++ SQL/tablesCreation.sql
@@ -15,7 +15,7 @@ CREATE TABLE business
     
     PRIMARY KEY (BusinessID),
     UNIQUE (Email),
-    FOREIGN KEY (Sector) REFERENCES Sectors(Name)
+    FOREIGN KEY (Sector) REFERENCES sectors(Name)
 );
 CREATE TABLE establishments
 (
@@ -23,7 +23,7 @@ CREATE TABLE establishments
     BusinessID VARCHAR(15) NOT NULL,    
     Location VARCHAR(50) NOT NULL,
     
-    FOREIGN KEY (BusinessID) REFERENCES Business (BusinessID),
+    FOREIGN KEY (BusinessID) REFERENCES business (BusinessID),
     PRIMARY KEY (EstablishmentID)  
 );
 CREATE TABLE individuals
@@ -35,7 +35,7 @@ CREATE TABLE individuals
     EncryptedPassword VARCHAR(200) NOT NULL,
     BusinessID VARCHAR(15) NULL,
     
-    FOREIGN KEY (BusinessID) REFERENCES Business(BusinessID),
+    FOREIGN KEY (BusinessID) REFERENCES business(BusinessID),
     PRIMARY KEY (Email)
 );
 CREATE TABLE appointments
@@ -46,8 +46,9 @@ CREATE TABLE appointments
     Time TIME NOT NULL,
     Location INT UNSIGNED NOT NULL,
     
-    FOREIGN KEY (Email) REFERENCES Individuals(Email),
-    FOREIGN KEY (BusinessID) REFERENCES Business(BusinessID),
-    FOREIGN KEY (Location) REFERENCES Establishments(EstablishmentID),
+    FOREIGN KEY (Email) REFERENCES individuals(Email),
+    FOREIGN KEY (BusinessID) REFERENCES business(BusinessID),
+    FOREIGN KEY (Location) REFERENCES establishments(EstablishmentID),
     PRIMARY KEY (Email, BusinessID)
 );
+
