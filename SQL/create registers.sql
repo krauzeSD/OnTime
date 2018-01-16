0c98d554d124ce17259b535505a097be1d484c8e
javimarinbergas <javiermarinbergas@gmail.com>
2018-01-16 18:49:38 +0100

    Create 3 registers for business

---
+++ SQL/create registers.sql
@@ -0,0 +1,3 @@
+insert into business values ('1234','Pepe','pepe@gmail.com','971601811','Mecanico');
+insert into business values ('1235','Javi','javi@gmail.com','971601834','Dentista');
+insert into business values ('1236','Alvaro','alvaro@gmail.com','971601834','Profesor');
