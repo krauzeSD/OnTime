ed0a48321db232feadeacc1c4c1f01f3afe817cc
Álvaro Tihanyi <34547477+krauzeSD@users.noreply.github.com>
2018-01-21 16:44:49 +0100

    Delete create registers.sql

--- SQL/create registers.sql
+++
@@ -1,6 +0,0 @@
-insert into sectors values ('Motion');
-insert into sectors values ('Health');
-insert into sectors values ('Education');
-insert into business values ('1234','Pepe','pepe@gmail.com','971601811','Motion');
-insert into business values ('1235','Javi','javi@gmail.com','971601834','Health');
-insert into business values ('1236','Alvaro','alvaro@gmail.com','971601834','Education');
