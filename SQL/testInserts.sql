6cb11588be9ed1e3d7d8d8610241e1db415abe4c
Álvaro Tihanyi <34547477+krauzeSD@users.noreply.github.com>
2018-01-21 16:46:40 +0100

    Update testInserts.sql

--- SQL/testInserts.sql
+++ SQL/testInserts.sql
@@ -4,3 +4,6 @@ insert into sectors values ('Education');
 insert into business values ('1234','Pepe','pepe@gmail.com','971601811','Motion');
 insert into business values ('1235','Javi','javi@gmail.com','971601834','Health');
 insert into business values ('1236','Alvaro','alvaro@gmail.com','971601834','Education');
+INSERT INTO establishments (Owner, Location) VALUES ('Javi', 'c/ Jaime III, 24')
+INSERT INTO establishments (Owner, Location) VALUES ('Pepe', 'c/ Aragón, 44')
+INSERT INTO establishments (Owner, Location) VALUES ('Alvaro', 'Plaza de España, 12')
